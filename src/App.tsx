ab42894a843d3e6543e7e7dc5516be6cc59168a2
José Vitor <jose-vitor-7@hotmail.com>
2022-05-02 23:03:34 -0300

    Bills per month

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
+import { months } from './services/data'
+
 interface IBill {
   id: number
   label: string
   value: number
+  month: number
 }
 
 export const App = () => {
   const [label, setLabel] = React.useState('')
   const [value, setValue] = React.useState(0)
+  const [month, setMonth] = React.useState(-1)
   const [bills, setBills] = React.useState<IBill[]>([])
   const [total, setTotal] = React.useState(0)
 
@@ -24,11 +28,17 @@ export const App = () => {
     }
   }
 
+  const handleMonthChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    if (Number(event.currentTarget.value) !== month) {
+      setMonth(Number(event.currentTarget.value))
+    }
+  }
+
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
     if (!!label.trim() && !!value) {
-      setBills(bills => [...bills, { id: bills.length, label, value }])
+      setBills(bills => [...bills, { id: bills.length, label, value, month }])
   
       setLabel('')
       setValue(0)
@@ -41,31 +51,53 @@ export const App = () => {
 
   React.useEffect(() => {
     if (window) {
-      const item = window.localStorage.getItem('bills')
+      const billsItem = window.localStorage.getItem('workaround.bills')
 
-      const data = item ? JSON.parse(item) : []
+      const billData = billsItem ? JSON.parse(billsItem) : []
 
-      setBills(data)
+      setBills(billData)
+      
+      const monthItem = window.localStorage.getItem('workaround.month')
+
+      const monthData = monthItem ? Number(JSON.parse(monthItem)) : 0
+
+      setMonth(monthData)
     }
   }, [])
 
   React.useEffect(() => {
-    if (window && bills.length > 0) {
-      window.localStorage.setItem('bills', JSON.stringify(bills))    
+    if (window && bills?.length > 0) {
+      window.localStorage.setItem('workaround.bills', JSON.stringify(bills))    
     }
   }, [bills])
 
   React.useEffect(() => {
-    if (bills?.length > 0) {
-      setTotal(bills.reduce((accumulator, bill) => accumulator + bill.value, 0))
+    if (window && month !== -1) {
+      window.localStorage.setItem('workaround.month', JSON.stringify(month))
     }
-  }, [bills])
+  }, [month])
+
+  React.useEffect(() => {
+    if (window && bills?.length > 0) {
+      setTotal(
+        bills.reduce(
+          (accumulator, bill) =>
+            bill.month === month
+              ? accumulator + bill.value
+              : accumulator,
+        0)
+      )
+    }
+  }, [bills, month])
 
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
         <div>
-          <label htmlFor="label">Nome da conta</label>
+          <h1>Adicionar conta</h1>
+        </div>
+        <div>
+          <label htmlFor="label">Nome</label>
           <input
             id="label"
             type="text"
@@ -83,23 +115,46 @@ export const App = () => {
           />
         </div>
         <div>
+          <label htmlFor="month">Mês</label>
+          <select id="month" value={month} onChange={handleMonthChange}>
+            {months.map(month => (
+              <option key={month.id} value={month.id}>
+                {month.label}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
           <button type="submit">Salvar</button>
         </div>
       </form>
       
-      {bills?.length > 0 && (
         <div>
-          <h2>Lista de contas ({total.toLocaleString('pt-BR')} R$)</h2>
-          <ul>
-            {bills?.map((bill) => (
-              <li key={bill.id}>
-                {bill.label} - {bill.value}
-                <button onClick={() => handleRemoveBill(bill.id)}>Remover</button>
-              </li>
-            ))}
-          </ul>
+          <h1>Lista de contas</h1>
+          <div>
+            <label htmlFor="month-l">Mês</label>
+            <select id="month-l" value={month} onChange={handleMonthChange}>
+              {months.map(month => (
+                <option key={month.id} value={month.id}>
+                  {month.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <p>Total: ({total.toLocaleString('pt-BR')} R$)</p>
+          </div>
+          {bills?.length > 0 && (
+            <ul>
+              {bills?.map((bill) => bill.month === month && (
+                <li key={bill.id}>
+                  {bill.label} - {bill.value.toLocaleString('pt-BR')} R$
+                  <button onClick={() => handleRemoveBill(bill.id)}>Remover</button>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
-      )}
     </div>
   )
 }
---
+++ src/services/data.ts
@@ -0,0 +1,14 @@
+export const months = [
+   { id: 0, label: 'Janeiro' },
+   { id: 1, label: 'Fevereiro' },
+   { id: 2, label: 'Março' },
+   { id: 3, label: 'Abril' },
+   { id: 4, label: 'Maio' },
+   { id: 5, label: 'Junho' },
+   { id: 6, label: 'Julho' },
+   { id: 7, label: 'Agosto' },
+   { id: 8, label: 'Setembro' },
+   { id: 9, label: 'Outubro' },
+   { id: 10, label: 'Novembro' },
+   { id: 11, label: 'Dezembro' }
+]
